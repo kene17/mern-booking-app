bb98dab4d96977f0e787da0a9505461d91927d8c
kene Ojukwu <keneojukwu@kenes-MacBook-Pro.local>
2024-05-11 19:50:34 -0400

    added search and filtering logic

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -116,6 +116,11 @@ export type SearchParams = {
   adultCount?: string;
   childCount?: string;
   page?: string;
+  facilities?: string[];
+  types?: string[];
+  stars?: string[];
+  maxPrice?: string;
+  sortOption?: string;
 };
 
 export const searchHotels = async (
@@ -123,17 +128,29 @@ export const searchHotels = async (
 ): Promise<HotelSearchResponse> => {
   const queryParams = new URLSearchParams();
   queryParams.append('destination', searchParams.destination || '');
-  queryParams.append('page', searchParams.page || '');
-  queryParams.append('childCount', searchParams.childCount || '');
-  queryParams.append('adultCount', searchParams.adultCount || '');
-  queryParams.append('checkOut', searchParams.checkOut || '');
   queryParams.append('checkIn', searchParams.checkIn || '');
+  queryParams.append('checkOut', searchParams.checkOut || '');
+  queryParams.append('adultCount', searchParams.adultCount || '');
+  queryParams.append('childCount', searchParams.childCount || '');
+  queryParams.append('page', searchParams.page || '');
+
+  queryParams.append('maxPrice', searchParams.maxPrice || '');
+  queryParams.append('sortOption', searchParams.sortOption || '');
+
+  searchParams.facilities?.forEach((facility) =>
+    queryParams.append('facilities', facility)
+  );
+
+  searchParams.types?.forEach((type) => queryParams.append('types', type));
+  searchParams.stars?.forEach((star) => queryParams.append('stars', star));
 
   const response = await fetch(
     `${API_BASE_URL}/api/hotels/search?${queryParams}`
   );
+
   if (!response.ok) {
-    throw new Error('Failed to fetch hotels');
+    throw new Error('Error fetching hotels');
   }
+
   return response.json();
 };
